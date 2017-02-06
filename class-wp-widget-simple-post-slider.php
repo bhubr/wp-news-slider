f71ff15d962dd1f6c7c93f94978d764e0f775f11
Benoît Hubert <benoithubert@gmail.com>
2017-02-06 21:26:51 +0100

    Converting code to WP coding standard (80% done)

--- class-wp-widget-simple-post-slider.php
+++ class-wp-widget-simple-post-slider.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Class for the actual Post Slider Widget
+ */
 require_once 'vendor/autoload.php';
 
 /**
@@ -6,136 +9,170 @@ require_once 'vendor/autoload.php';
  */
 class WP_Widget_Simple_Post_Slider extends WP_Widget {
 
-    function __construct() {
-        $widget_ops = array('classname' => 'widget_news_slider', 'description' => __( "The most recent posts on your site") );
-        parent::__construct('news-slider', __( 'News Slider', 'wpnsw' ), $widget_ops);
-        $this->alt_option_name = 'widget_news_slider';
+	/**
+	 * Constructor
+	 *
+	 * Initialize stuff, register WP actions
+	 */
+	function __construct() {
+		$widget_ops = array( 'classname' => 'widget_news_slider', 'description' => __( 'The most recent posts on your site' ) );
+		parent::__construct( 'simple-post-slider', __( 'Simple Post Slider', 'wpnsw' ), $widget_ops );
+		$this->alt_option_name = 'widget_news_slider';
 
-        add_action( 'save_post', array(&$this, 'flush_widget_cache') );
-        add_action( 'deleted_post', array(&$this, 'flush_widget_cache') );
-        add_action( 'switch_theme', array(&$this, 'flush_widget_cache') );
-    }
+		add_action( 'save_post', array( &$this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( &$this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( &$this, 'flush_widget_cache' ) );
+	}
 
-    function widget($args, $instance) {
-        $cache = wp_cache_get('widget_news_slider', 'widget');
+	/**
+	 * Outputs widget
+	 * @param array arguments for this instance
+	 * @param string instance the instance
+	 */
+	function widget($args, $instance) {
+		$cache = wp_cache_get( 'widget_news_slider', 'widget' );
 
-        if ( !is_array($cache) )
-            $cache = array();
+		if ( ! is_array( $cache ) ) {
+			$cache = array();
+		}
 
-        if ( isset($cache[$args['widget_id']]) ) {
-            echo $cache[$args['widget_id']];
-            return;
-        }
+		if ( isset( $cache[ $args['widget_id'] ] ) ) {
+			echo $cache[ $args['widget_id'] ];
+			return;
+		}
 
-        ob_start();
-        extract($args);
+		$output = '';
+		extract( $args );
 
-        $title = apply_filters('widget_title', empty($instance['title']) ? __('News Slider', 'wpnsw' ) : $instance['title'], $instance, $this->id_base);
-        
-        $number = get_sanitized_post_number( (int) $instance['number'] ); 
-        /* Query Recent Posts */
-        $query = new WP_Query(array('showposts' => $number, 'nopaging' => 0, 'post_status' => 'publish', 'caller_get_posts' => 1));
-        
-        if ( !$query->have_posts() ) return;
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'News Slider', 'wpnsw' ) : $instance['title'], $instance, $this->id_base );
 
-        /* Recompute posts number */
-        $recent_posts = $query->get_posts();
-        $post_count = count( $recent_posts );
-        if( $post_count < $number ) $number = $post_count;
+		$number = get_sanitized_post_number( (int) $instance['number'] );
+		/* Query Recent Posts */
+		$query = new WP_Query( array( 'showposts' => $number, 'nopaging' => 0, 'post_status' => 'publish', 'caller_get_posts' => 1 ) );
 
-        echo $before_widget;
-        if( !empty( $instance['title_is_link'] ) ) $title = convert_title_link( $title );
-        if ( !empty( $title ) ) echo $before_title . $title . $after_title;
+		if ( ! $query->have_posts() ) {
+			return;
+		}
 
-        $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
-        $twig = new Twig_Environment($loader, array(
-            //'cache' => __DIR__ . '/compilation_cache',
-        ));
+		/* Recompute posts number */
+		$recent_posts = $query->get_posts();
+		$post_count = count( $recent_posts );
+		if ( $post_count < $number ) {
+			$number = $post_count;
+		}
+		$output .= $before_widget;
+		if ( ! empty( $instance['title_is_link'] ) ) {
+			$title = convert_title_link( $title );
+		}
+		if ( ! empty( $title ) ) {
+			$output .= $before_title . $title . $after_title;
+		}
 
-        $posts = array_map(function($post) {
-            return array(
-                'title'     => $post->post_title,
-                'permalink' => get_the_permalink($post),
-                'excerpt'   => post_html_excerpt($post->post_content),
-                'thumbnail' => get_the_post_thumbnail($post, 'thumbnail')
-            );
-        }, $recent_posts);
+		$loader = new Twig_Loader_Filesystem( __DIR__ . '/templates' );
+		$twig = new Twig_Environment($loader, array(
+			// 'cache' => __DIR__ . '/compilation_cache',
+		) );
 
-        $strings = array(
-            'stop' => __( 'Stop', 'wpnsw' ),
-            'play' => __( 'Play &gt;', 'wpnsw' ),
-            'prev' => __( '&lt;&lt; Previous', 'wpnsw' ),
-            'next' => __( 'Next &gt;&gt;', 'wpnsw' )
-        );
-        $view = array(
-            'id'      => $this->id,
-            'posts'   => $posts,
-            'str'     => $strings,
-            'options' => json_encode($instance)
-        );
-        echo $twig->render('template.twig.html', $view);
-        echo $after_widget;
+		$posts = array_map( function( $post ) {
+			return array(
+				'title'     => $post->post_title,
+				'permalink' => get_the_permalink( $post ),
+				'excerpt'   => post_html_excerpt( $post->post_content ),
+				'thumbnail' => get_the_post_thumbnail( $post, 'thumbnail' ),
+			);
+		}, $recent_posts );
 
-        // Reset the global $the_post as this query will have stomped on it
-        wp_reset_postdata();
+		$strings = array(
+			'stop' => __( 'Stop', 'wpnsw' ),
+			'play' => __( 'Play &gt;', 'wpnsw' ),
+			'prev' => __( '&lt;&lt; Previous', 'wpnsw' ),
+			'next' => __( 'Next &gt;&gt;', 'wpnsw' ),
+		);
+		$view = array(
+			'id'      => $this->id,
+			'posts'   => $posts,
+			'str'     => $strings,
+			'options' => json_encode( $instance ),
+		);
+		$output .= $twig->render( 'template.twig.html', $view );
+		$output .= $after_widget;
+		// Reset the global $the_post as this query will have stomped on it.
+		wp_reset_postdata();
 
-        $cache[$args['widget_id']] = ob_get_flush();
-        wp_cache_set('widget_news_slider', $cache, 'widget');
-    }
-    
-        static function register(){
-        register_widget('WP_Widget_Simple_Post_Slider');
-    }
+		$cache[ $args['widget_id'] ] = $output;
+		wp_cache_set( 'widget_simple_post_slider', $cache, 'widget' );
+		echo $output;
+	}
 
-    function update( $new_instance, $old_instance ) {
+	/**
+	 * Register the widget
+	 */
+	static function register(){
+		register_widget( 'WP_Widget_Simple_Post_Slider' );
+	}
 
-        $instance = $old_instance;
-        $instance = array( 'title_is_link' => 0, 'show_thumbs' => 0 );
-        foreach ( $instance as $field => $val ) {
-            if ( isset($new_instance[$field]) )
-                $instance[$field] = 1;
-        }
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['number'] = (int) $new_instance['number'];
-        $instance['interval'] = ( !empty( $new_instance['interval'] ) ? (int) $new_instance['interval'] : 5 );
-        
-        $this->flush_widget_cache();
+	/**
+	 * Update widget instance options
+	 */
+	function update( $new_instance, $old_instance ) {
 
-        $alloptions = wp_cache_get( 'alloptions', 'options' );
-        if ( isset($alloptions['widget_news_slider']) )
-            delete_option('widget_news_slider');
+		$instance = $old_instance;
+		$instance = array( 'title_is_link' => 0, 'show_thumbs' => 0 );
+		foreach ( $instance as $field => $val ) {
+			if ( isset( $new_instance[ $field ] ) ) {
+				$instance[ $field ] = 1;
+			}
+		}
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['number'] = (int) $new_instance['number'];
+		$instance['interval'] = ( ! empty( $new_instance['interval'] ) ? (int) $new_instance['interval'] : 5 );
 
-        return $instance;
-    }
+		$this->flush_widget_cache();
 
-    function flush_widget_cache() {
-        wp_cache_delete('widget_news_slider', 'widget');
-    }
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset( $alloptions['widget_news_slider'] ) ) {
+			delete_option( 'widget_news_slider' );
+		}
 
-    function form( $instance ) {
-        $instance = wp_parse_args( (array) $instance, array( 'title' => "", 'number' => 0, 'interval' => 0, 'show_thumbs' => false, 'title_is_link' => false ) );
+		return $instance;
+	}
 
-        $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
-        if ( !isset($instance['number']) || !$number = (int) $instance['number'] )
-            $number = 5;
-        if ( !isset($instance['interval']) || !$interval = (int) $instance['interval'] )
-            $interval = 5;
+	/**
+	 * Yet another bloody thing that I don't remind of
+	 */
+	function flush_widget_cache() {
+		wp_cache_delete( 'widget_news_slider', 'widget' );
+	}
+
+	/**
+	 * Options form for this widget instance
+	 */
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => "", 'number' => 0, 'interval' => 0, 'show_thumbs' => false, 'title_is_link' => false ) );
+
+		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		if ( !isset( $instance['number'] ) || !$number = (int) $instance['number'] ) {
+			$number = 5;
+		}
+		if ( !isset($instance['interval']) || !$interval = (int) $instance['interval'] ) {
+			$interval = 5;
+		}
 ?>
-        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
 
-        <p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
-        <input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="2" /></p>
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
+		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="2" /></p>
 
-        <p><label for="<?php echo $this->get_field_id('interval'); ?>"><?php _e('Slide interval (seconds):', 'wpnsw' ); ?></label>
-        <input id="<?php echo $this->get_field_id('interval'); ?>" name="<?php echo $this->get_field_name('interval'); ?>" type="text" value="<?php echo $interval; ?>" size="3" /></p>
+		<p><label for="<?php echo $this->get_field_id('interval'); ?>"><?php _e('Slide interval (seconds):', 'wpnsw' ); ?></label>
+		<input id="<?php echo $this->get_field_id('interval'); ?>" name="<?php echo $this->get_field_name('interval'); ?>" type="text" value="<?php echo $interval; ?>" size="3" /></p>
 
-        <input class="checkbox" type="checkbox" <?php checked($instance['title_is_link'], true) ?> id="<?php echo $this->get_field_id('title_is_link'); ?>" name="<?php echo $this->get_field_name('title_is_link'); ?>" />
-        <label for="<?php echo $this->get_field_id('title_is_link'); ?>"><?php _e('Turn widget title into a link', 'wpnsw' ); ?></label><br />
+		<input class="checkbox" type="checkbox" <?php checked($instance['title_is_link'], true) ?> id="<?php echo $this->get_field_id('title_is_link'); ?>" name="<?php echo $this->get_field_name('title_is_link'); ?>" />
+		<label for="<?php echo $this->get_field_id('title_is_link'); ?>"><?php _e('Turn widget title into a link', 'wpnsw' ); ?></label><br />
 
-        <input class="checkbox" type="checkbox" <?php checked($instance['show_thumbs'], true) ?> id="<?php echo $this->get_field_id('show_thumbs'); ?>" name="<?php echo $this->get_field_name('show_thumbs'); ?>" />
-        <label for="<?php echo $this->get_field_id('show_thumbs'); ?>"><?php _e('Show post thumbnails', 'wpnsw' ); ?></label><br />
+		<input class="checkbox" type="checkbox" <?php checked($instance['show_thumbs'], true) ?> id="<?php echo $this->get_field_id('show_thumbs'); ?>" name="<?php echo $this->get_field_name('show_thumbs'); ?>" />
+		<label for="<?php echo $this->get_field_id('show_thumbs'); ?>"><?php _e('Show post thumbnails', 'wpnsw' ); ?></label><br />
 
-        <?php
-    }
+		<?php
+	}
 }
