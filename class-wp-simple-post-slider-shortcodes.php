7926d8fdec1b26b18754ba9adaf755cdd86b2159
Benoît Hubert <benoithubert@gmail.com>
2017-02-27 03:20:35 +0100

    clean-up

--- class-wp-simple-post-slider-shortcodes.php
+++ class-wp-simple-post-slider-shortcodes.php
@@ -30,27 +30,16 @@ class WP_Simple_Post_Slider_Shortcodes {
 		$number = isset($args['number']) ? $args['number'] : 5;
 		$post_type = isset($args['post_type']) ? $args['post_type'] : 'attachment';
 		$image_ids = array_map(function($id) { return (int)$id; }, explode(',', $args['images']));
-		// var_dump($post_type);
-		// var_dump($image_ids);
-		/* Query Recent Posts */
+
 		global $wpdb;
 		$recent_posts = $wpdb->get_results(
 			"SELECT ID,guid from $wpdb->posts WHERE ID IN (" . $args['images'] . ") ORDER BY ID DESC"
 		);
-		// $recent_posts = get_posts( array(
-		// 	'post__in ' => $image_ids,
-		// 	// 'showposts' => $number,
-		// 	// 'nopaging' => 0,
-		// 	'post_status' => 'inherit',
-		// 	// 'caller_get_posts' => 1,
-		// 	'post_type' => $post_type
-		// ) );
 		if ( empty( $recent_posts ) ) {
 			return;
 		}
 
 		/* Recompute posts number */
-		// $recent_posts = $query->get_posts();
 		$post_count = count( $recent_posts );
 		if ( $post_count < $number ) {
 			$number = $post_count;
@@ -59,10 +48,6 @@ class WP_Simple_Post_Slider_Shortcodes {
 		/* Setup posts before sending them to Twig view */
 		$posts = array_map( function( $post ) {
 			return array(
-				// 'id' => $post->ID,
-				// 'title'     => $post->post_title,
-				// 'permalink' => get_the_permalink( $post ),
-				// 'excerpt'   => post_html_excerpt( $post->post_content ),
 				'thumbnail' => "<img id=\"image-{$post->ID}\" class=\"pure-img\" src=\"{$post->guid}\" />",
 			);
 		}, $recent_posts );
@@ -74,7 +59,6 @@ class WP_Simple_Post_Slider_Shortcodes {
 		}
 
 		$view = array(
-			'id'      => $this->id,
 			'posts'   => $posts,
 			'options' => $args,
 			'jsonOptions' => json_encode( $args ),
